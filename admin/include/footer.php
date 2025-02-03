b73e7457e7ebaf2b88dace131919df9951995f52
momviseu software developer <momviseu@gmail.com>
2025-02-03 16:51:47 +0700

    1

---
+++ admin/include/footer.php
@@ -0,0 +1,40 @@
+<footer class="tm-footer row tm-mt-small">
+    <div class="col-12 font-weight-light">
+        <p class="text-center text-white mb-0 px-4 small">
+            Copyright &copy; <b>2018</b> All rights reserved. 
+            
+            Design: <a rel="nofollow noopener" href="https://templatemo.com" class="tm-footer-link">Template Mo</a>
+        </p>
+    </div>
+</footer>
+
+<script src="../asset/js/jquery-3.3.1.min.js"></script>
+<script src="../asset/js/moment.min.js"></script>
+<script src="../asset/js/Chart.min.js"></script>
+<script src="../asset/js/bootstrap.min.js"></script>
+<script src="../asset/js/tooplate-scripts.js"></script>
+<script>
+    Chart.defaults.global.defaultFontColor = 'white';
+    let ctxLine,
+        ctxBar,
+        ctxPie,
+        optionsLine,
+        optionsBar,
+        optionsPie,
+        configLine,
+        configBar,
+        configPie,
+        lineChart;
+    barChart, pieChart;
+    // DOM is ready
+    $(function () {
+        drawLineChart(); // Line Chart
+        drawBarChart(); // Bar Chart
+        drawPieChart(); // Pie Chart
+
+        $(window).resize(function () {
+            updateLineChart();
+            updateBarChart();                
+        });
+    })
+</script>
